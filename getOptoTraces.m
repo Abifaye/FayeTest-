e9a2d005392b9dff3e3d57ff792fffa3694c47b4
Faye <ab.arellano@ucalgary.ca>
2022-10-06 15:20:54 -0600

    Edited optoTraces
    added a matrix to put in the traces

--- getOptoTraces.m
+++ getOptoTraces.m
@@ -1,11 +1,13 @@
-function [optoStimuli] = getOptoTraces(trials)
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
+function [traceMatrix] = getOptoTraces(trials)
+%Written by Faye 220929
+%Edited 221006
 
-
+tic;
+%init the matrix for traces
+traceMatrix = zeros(length(trials),8000);
 
 % init your output (Best Practice)
-optoStimuli = cell(length(trials),1);
+% optoStimuli = cell(length(trials),1);
 
 % For each trial
 for nTrials = 1:length(trials)
@@ -17,10 +19,10 @@ for nTrials = 1:length(trials)
     % Created a manual counter
     counter = 1;
     % Init a place to create the trace (Best Practice)
-    optoTrace = zeros(1,max(time) - min(time));
+    optoTrace = zeros(1,range(time));
     
     % For time change point
-    for timePoints = 2:length(time);
+    for timePoints = 2:length(time)
         % How many timepoints elapsed between the current point and the
         % next time point
         nBins = time(timePoints) - time(timePoints-1);
@@ -33,7 +35,26 @@ for nTrials = 1:length(trials)
     end
     % You've completed a loop through one trial --> Write the result to the
     % output that you will return from the function
-    optoStimuli{nTrials,1} = optoTrace;
+    % optoStimuli{nTrials,1} = optoTrace;
+    % input optoTrace into the matrix, matrix must be same length as
+    % optoTrace and spans from starting point ( 0 = 4000) until duration
+    traceMatrix(nTrials,min(time)+4000:min(time)+4000+range(time)) = optoTrace;
+end
+toc;
 end
 
-end
+
+%%
+
+% For each trial, 
+% if there is no power at all 
+% (trials.meanPowerMW ==0) skip this trial (continue)
+% if there is power (trials.meanPowerMW ~= 0)
+% Then you will start to edit the matrix, given current time point
+% However:
+% if no power in current time window, you can skip those bins
+% else you need to write the power for that time window
+% Tricky part: to the correct bins in traceMatrix
+% Account for the reference/start point for your trace Matrix
+% and the start point of the optoProfile on that trial.
+
---
+++ getpower.m
@@ -0,0 +1,5 @@
+function [power,time] = getpower (trials)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+power = [trials.optoStepPowerMW];
+end
