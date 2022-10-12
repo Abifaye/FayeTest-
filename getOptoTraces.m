676e0695da81ef4d9a23326ab8520ce3c1838908
Faye <ab.arellano@ucalgary.ca>
2022-10-12 12:36:29 -0600

    Edited getOptoTraces
    cleaned up code and renamed

--- getOptoTraces.m
+++ getOptoTraces.m
@@ -1,13 +1,13 @@
 function [traceMatrix] = getOptoTraces(trials);
 %Written by Faye 220929
 %Edited 221006
+%Edited 221012
+%Get the power and time from trials. Throughout time in each trial, put the
+%corresponding power. This is placed in a trace called traceMatrix
 
 tic;
 %init the matrix for traces
 traceMatrix = zeros(length(trials),8000);
-
-% init your output (Best Practice)
-% optoStimuli = cell(length(trials),1);
 
 % For each trial
 for nTrials = 1:length(trials)
@@ -15,48 +15,29 @@ for nTrials = 1:length(trials)
     % Extract time change points and powers presented
     time = trials(nTrials).optoStepTimesMS;
     power = trials(nTrials).optoStepPowerMW;
-    
-    % Created a manual counter
-    counter = 1;
-    % Init a place to create the trace (Best Practice)
-    optoTrace = zeros(1,range(time));
-    
-    % For time change point
-    for timePoints = 2:length(time)
-        % How many timepoints elapsed between the current point and the
-        % next time point
-        nBins = time(timePoints) - time(timePoints-1);
-        % Write the power to the corresponding duration
-        optoTrace(1,counter:counter+nBins) = power(timePoints-1);
-        % Manually adjust our counter to the new start point for next
-        % bin
-        counter = counter + nBins;
+    meanPower = trials(nTrials).meanPowerMW;
 
+    if meanPower==0 % If there was no opto on this trial, skip
+        continue;
+    elseif  meanPower~=0 % If there is opto, iterate through the trace
+        % Loop through all time bins
+        for timePoints = 2:length(time)
+            if power(timePoints-1)==0 % If there is no power during this bin, skip
+                continue  
+            elseif power(timePoints-1)~=0 % If power, write the power to the bins that correspond to the right times
+                traceMatrix(nTrials,time(timePoints-1)+4000:time(timePoints)+4000) = power(timePoints-1);
+            end
+        end
     end
-    % You've completed a loop through one trial --> Write the result to the
-    % output that you will return from the function
-    % optoStimuli{nTrials,1} = optoTrace;
-    % input optoTrace into the matrix, matrix must be same length as
-    % optoTrace and spans from starting point ( 0 = 4000) until duration
-    traceMatrix(nTrials,min(time)+4000:min(time)+4000+range(time)) = optoTrace;
 end
 toc;
 end
 
 
-%%
 
-% For each trial, 
-% if there is no power at all 
-% (trials.meanPowerMW ==0) skip this trial (continue)
-% if there is power (trials.meanPowerMW ~= 0)
-% Then you will start to edit the matrix, given current time point
-% However:
-% if no power in current time window, you can skip those bins
-% else you need to write the power for that time window
-% Tricky part: to the correct bins in traceMatrix
-% Account for the reference/start point for your trace Matrix
-% and the start point of the optoProfile on that trial.
-%build heatmaps for differences btwn code and each trials (hits, misses)
-%mean of both matrix 2 versions of code
+
+
+
+
+
 
--- getOptoTracesOld.m
+++ getOptoTracesOld.m
@@ -1,12 +1,13 @@
-function [traceMatrix] = getOptoTracesV2(trials);
+function [traceMatrix] = getOptoTracesOld(trials);
 %Written by Faye 220929
 %Edited 221006
+
 tic;
 %init the matrix for traces
 traceMatrix = zeros(length(trials),8000);
 
 % init your output (Best Practice)
-%optoStimuli = cell(length(trials),1);
+% optoStimuli = cell(length(trials),1);
 
 % For each trial
 for nTrials = 1:length(trials)
@@ -14,34 +15,30 @@ for nTrials = 1:length(trials)
     % Extract time change points and powers presented
     time = trials(nTrials).optoStepTimesMS;
     power = trials(nTrials).optoStepPowerMW;
-    meanPower = trials(nTrials).meanPowerMW;
     
     % Created a manual counter
     counter = 1;
     % Init a place to create the trace (Best Practice)
-    %optoTrace = zeros(1,range(time));
- if meanPower==0
-     continue;
- elseif  meanPower~=0
+    optoTrace = zeros(1,range(time));
+    
     % For time change point
     for timePoints = 2:length(time)
         % How many timepoints elapsed between the current point and the
         % next time point
         nBins = time(timePoints) - time(timePoints-1);
         % Write the power to the corresponding duration
-        if  power(timePoints-1)==0
-            continue
-        elseif power(timePoints-1)~=0
-            traceMatrix(nTrials,time(timePoints-1)+4000:time(timePoints)+4000) = power(timePoints-1);
-            % Manually adjust our counter to the new start point for next
-            % bin
-            counter = counter + nBins;
-        end
+        optoTrace(1,counter:counter+nBins) = power(timePoints-1);
+        % Manually adjust our counter to the new start point for next
+        % bin
+        counter = counter + nBins;
+
     end
- end
     % You've completed a loop through one trial --> Write the result to the
     % output that you will return from the function
     % optoStimuli{nTrials,1} = optoTrace;
+    % input optoTrace into the matrix, matrix must be same length as
+    % optoTrace and spans from starting point ( 0 = 4000) until duration
+    traceMatrix(nTrials,min(time)+4000:min(time)+4000+range(time)) = optoTrace;
 end
 toc;
 end
@@ -58,21 +55,6 @@ end
 % if no power in current time window, you can skip those bins
 % else you need to write the power for that time window
 % Tricky part: to the correct bins in traceMatrix
-
-%% for weekend
 % Account for the reference/start point for your trace Matrix
 % and the start point of the optoProfile on that trial.
-%loop a single trial 
-
-%% heatmap
-
-heatmap(traceMatrix, 'colorVariable')
-hold on
-heatmap(traceMatrix,'colorVariable')
-hold off 
-
-
-
-
-
 
