786fa071c40a64d8130606b1a36f59831bf68869
Faye <ab.arellano@ucalgary.ca>
2022-10-13 14:31:32 -0600

    revised date function
    made it into 1 line

--- getdate.m
+++ getdate.m
@@ -1,11 +1,8 @@
 function [date] = getdate(pwd);
 %created by Faye 2210113
-% inputs to the current folder, finds file name, convert it to a
-% string, and finally sparse the date from file name
+% inputs to the current folder, finds file name, and take only the date from file name
 %pwd accesses current folder
-fileDir = dir('*.mat'); %access file name
-arrayFileName = struct2cell(fileDir); %converts file name from struct to cell
-strFileName =  string(arrayFileName); %converts cell to string
-delimiters = ".mat"; %set part to be removed from file name
-date = strtok(strFileName(1),delimiters); %date is left
+ %access file name
+date = erase(dir('*.mat').name,'.mat');
+
 end
