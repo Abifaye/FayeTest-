0dff0fdbb3cf02f8bb31a1fc4ba2b349743ddeed
Faye <ab.arellano@ucalgary.ca>
2022-10-13 12:45:30 -0600

    edited getdate
    used pwd for folder name

--- getdate.m
+++ getdate.m
@@ -1,10 +1,11 @@
-function [date] = getdate('/Users/mouseuser/Documents/GitHub/FayeTest-');
-%UNTITLED5 Summary of this function goes here
-%   Detailed explanation goes here
-
-fileDir = dir('*.mat');
-arrayFileName = struct2cell(fileDir);
-strFileName =  string(arrayFileName);
-delimiters = ".mat";
-date = strtok(strFileName(1),delimiters);
+function [date] = getdate(pwd);
+%created by Faye 2210113
+% inputs to the current folder, finds file name, convert it to a
+% string, and finally sparse the date from file name
+%pwd accesses current folder
+fileDir = dir('*.mat'); %access file name
+arrayFileName = struct2cell(fileDir); %converts file name from struct to cell
+strFileName =  string(arrayFileName); %converts cell to string
+delimiters = ".mat"; %set part to be removed from file name
+date = strtok(strFileName(1),delimiters); %date is left
 end
