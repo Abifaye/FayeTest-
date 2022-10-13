e33ad3d2644266b1a3685282c87aebb3f98e73f0
Faye <ab.arellano@ucalgary.ca>
2022-10-13 12:38:25 -0600

    revised getdate
    getdate finds file name and parses the date portion

--- getdate.m
+++ getdate.m
@@ -2,7 +2,9 @@ function [date] = getdate('/Users/mouseuser/Documents/GitHub/FayeTest-');
 %UNTITLED5 Summary of this function goes here
 %   Detailed explanation goes here
 
-dir '*.mat'
-
-date = strip('','both','.mat')
+fileDir = dir('*.mat');
+arrayFileName = struct2cell(fileDir);
+strFileName =  string(arrayFileName);
+delimiters = ".mat";
+date = strtok(strFileName(1),delimiters);
 end
