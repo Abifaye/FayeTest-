71a4d4927efe2e42c3967ddfba0758b75a1fd3d5
Faye <ab.arellano@ucalgary.ca>
2022-10-14 14:59:43 -0600

    edited holdTime

--- getRTs.m
+++ getRTs.m
@@ -1,4 +1,4 @@
-function [RTs] = getRTs(trials)
+function [RTs] = getRTs(trials);
 % Written by Faye 220928
 % Function takes session data from OKernel "trials" as input
 % returns an array of Reaction Times in ms
