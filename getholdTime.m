4ed2ad9d93f53d8100b417e9dab481f4da4834b0
Faye <ab.arellano@ucalgary.ca>
2022-10-14 15:14:32 -0600

    created comments on holdTime

--- getholdTime.m
+++ getholdTime.m
@@ -1,22 +1,24 @@
 function [holdTime] = getholdTime(trials);
-%
-% inputs into trials and get hold time,;
-preStimLoc =[trials.trial];
-preStimTime = [preStimLoc.preStimMS]; 
-responseType = [trials.trialEnd];
-RTs = [trials.reactTimeMS]
-totalHoldTime = preStimTime(responseType~=2) - RTs(responseType~=2);
+%Created by
+% inputs into trials and get hold time, and corrects for incorrect neg values
+% by adding 500
+preStimLoc =[trials.trial];%gets location of struct containing preStimTime
+preStimTime = [preStimLoc.preStimMS]; %gets preStimTime
+responseType = [trials.trialEnd]; %gets response types
+RTs = [trials.reactTimeMS]; %gets reaction time
+totalHoldTime = preStimTime(responseType~=2) - RTs(responseType~=2); %calculates
+%totalHoldTime by subtracting RTs that are not misses from the preStimTime
+%that are not misses
 
-holdTime = [];
-
-for i = 1:length(totalHoldTime)
-    if totalHoldTime(i) < 0
-        holdTime(i) = totalHoldTime(i) + 500;
-    else
-        holdTime(i) = totalHoldTime(i);
+for i = 1:length(totalHoldTime) %loops through totalHoldTime
+    if totalHoldTime(i) < 0 %when totalHoldTime is negative
+        holdTime(i) = totalHoldTime(i) + 500; %add 500 to the value and write it 
+        % in holdTime
+    else %or if it's positive
+        holdTime(i) = totalHoldTime(i); %just write the value in holdTime
     end
 end
-
+%check = totalHoldTime < 0; no neg values?
 end
 
 
