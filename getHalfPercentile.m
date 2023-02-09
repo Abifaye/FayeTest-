cf2e463a1dbad2c085216512171de5ae1efda195
Faye <115123218+Abifaye@users.noreply.github.com>
2023-02-08 19:30:43 -0700

    quartile

--- getHalfPercentile.m
+++ getHalfPercentile.m
@@ -1,19 +1,17 @@
 function [topPros,btmPros] = getHalfPercentile
-%Splits RTs into top and bottom half percentile and takes the corresponding
-%hit profiles
+% Splits RTs into top and bottom half percentile and takes the corresponding
+% hit profiles
 
 %% Initialize variables
 %Go to folder containing hit profiles
-cd 'E:\School\Psyc 504\FayeTest-'
+
+endcd 'C:\Users\afbar\OneDrive - University of Calgary\Documents\GitHub\FayeTest-'
 %load master table with hit profiles file 
 load('TablewithHitProfiles.mat');
 %getmasterRTs
 %init locations for top + btm profiles
-topPros = zeros();
-btmPros = zeros();
-%init counters for determining where to put profiles
-CounterTop = 1;
-CounterBtm = 1;
+topPros = [];
+btmPros = [];
 %% Create loop for getting hit profiles and putting them in topPros/btmPros matrix
 %loop through all sessions
 for nSession = 1:height(TablewithHitProfiles)
@@ -24,25 +22,16 @@ for nSession = 1:height(TablewithHitProfiles)
     %Create logical index for the top + btm percentile of RTs. prctile function
     %creates range for top and bottom percentile
     topIdx = (RTs >= min(prctile(RTs,[0 50])) & RTs <= max(prctile(RTs,[0 50])));
-    btmIdx = (RTs > min(prctile(RTs,[50.01 100])) & RTs <= max(prctile(RTs,[50.01 100])));
-    %loops through all trials in current session
-    
-    for nTrial = 1:length(RTs)
-        
-        %if current trial is in top half percentile
-        if topIdx(nTrial) == 1
-            %place corresponding hit profile in topPros matrix
-            topPros(CounterTop,1:width(hitPros)) = hitPros(nTrial);
-            %increase height of matrix if a profile gets added
-            CounterTop = 1 + height(topPros);
-           
-            %if current trial is in btm half percentile
-        elseif btmIdx (nTrial)  == 1
-            %place corresponding hit profile in btmPros matrix
-            btmPros(CounterBtm,1:width(hitPros)) = hitPros (nTrial);
-            %increase height of matrix if a profile gets added
-            CounterBtm =  1 + height(btmPros);
-        end
-    end
+    btmIdx = (RTs > min(prctile(RTs,[50 100])) & RTs <= max(prctile(RTs,[50 100])));
+    %Grabs all trials in current session and appends them to the matrix
+    topPros = [topPros; hitPros(topIdx,:)];
+    btmPros = [btmPros; hitPros(btmIdx,:)];
 end
+clf;
+hold on
+plot(mean(topPros/2 + 0.5,1),Color='b')
+plot(mean(btmPros/2 + 0.5,1),Color='r')
+hold off
+title('Half Percentile of Mean Hit Profiles')
+legend('Top Half Percentile','Bottom Half Percentile','Location','southwest')
 end
--- getQuartile.m
+++ getQuartile.m
@@ -1,55 +1,118 @@
-function [firstPros,secondPros,thirdPros,fourthPros] = getQuartile
+function [firstTertile,secondTertile,thirdTertile,fourthTertile] = getQuartile
 %Splits RTs into top and bottom half percentile and takes the corresponding
 %hit profiles
-%% Initialize variables
-%Go to folder containing hit profiles
-cd 'E:\School\Psyc 504\FayeTest-'
-%get hitProsloc function to get all hit profiles
-%hitPros = gethitProsLoc;
-%go back to previous folder to access getmasterRTs function
-%cd 'E:\School\Psyc 504\FayeTest-\Stim Profiles';
-%load masterTable file to be able to use getmasterRTs
-load('TablewithHitProfiles.mat');
-%init locations for top + btm profiles
-firstPros = zeros();
-secondPros = zeros();
-thirdPros = zeros();
-fourthPros = zeros();
-%init counters for determining where to put profiles
-Counterfirst = 1;
-Countersecond = 1;
-Counterthird = 1;
-Counterfourth = 1;
 
+%% Initialize variables
+cd 'C:\Users\afbar\OneDrive - University of Calgary\Documents\GitHub\FayeTest-'
+%load master table with hit profiles file 
+load('TablewithHitProfiles.mat');
+%getmasterRTs
+%init locations for top + btm profiles
+firstTertile = [];
+secondTertile = [];
+thirdTertile = [];
+fourthTertile = [];
 %% Create loop for getting hit profiles and putting them in topPros/btmPros matrix
+%loop through all sessions
 for nSession = 1:height(TablewithHitProfiles)
+    %create variables for hit profiles and reaction times from the master
+    %table
     RTs = cell2mat(TablewithHitProfiles.stimCorrectRTs(nSession));
     hitPros = cell2mat(struct2cell(TablewithHitProfiles.HitProfiles(nSession)));
     %Create logical index for the top + btm percentile of RTs. prctile function
+   
     %creates range for top and bottom percentile
-    firstIdx = (RTs >= min(prctile(RTs,[0 25])) & RTs <= max(prctile(RTs,[0 25])));
+     firstIdx = (RTs >= min(prctile(RTs,[0 25])) & RTs <= max(prctile(RTs,[0 25])));
     secondIdx =(RTs > min(prctile(RTs,[25.01 50])) & RTs <= max(prctile(RTs,[25.01 50])));
     thirdIdx = (RTs > min(prctile(RTs,[50.01 75])) & RTs <= max(prctile(RTs,[50.01 75])));
     fourthIdx = (RTs > min(prctile(RTs,[75.01 100])) & RTs <= max(prctile(RTs,[75.01 100])));
-    for nTrial = 1:length(RTs) %loops through all hit profiles
-        if firstIdx(nTrial) == 1 %if current trial is in top half percentile
-            firstPros(Counterfirst,1:width(hitPros)) = hitPros(nTrial); %place
-            %corresponding hit profile in topPros matrix
-            Counterfirst = 1 + height(firstPros); %increase height of matrix if a profile gets added
-        elseif secondIdx(nTrial) == 1
-            secondPros(Countersecond,1:width(hitPros)) = hitPros(nTrial); %place
-            %corresponding hit profile in topPros matrix
-            Countersecond = 1 + height(secondPros);
-        elseif thirdIdx (nTrial)  == 1 %if current trial is in btm half percentile
-            thirdPros(Counterthird,1:width(hitPros)) = hitPros(nTrial); %place
-            %corresponding hit profile in btmPros matrix
-            Counterthird =  1 + height(thirdPros); %increase height of matrix if a profile gets added
-        elseif fourthIdx (nTrial)  == 1 %if current trial is in btm half percentile
-            fourthPros(Counterfourth,1:width(hitPros)) = hitPros(nTrial); %place
-            %corresponding hit profile in btmPros matrix
-            Counterfourth =  1 + height(fourthPros); %increase height of matrix if a profile gets added
-        end
-    end
+    
+    %Grabs all trials in current session and appends them to the matrix
+    firstTertile = [firstTertile; hitPros(firstIdx,:)];
+    secondTertile = [secondTertile; hitPros(secondIdx,:)];
+    thirdTertile = [thirdTertile; hitPros(thirdIdx,:)];
+    fourthTertile = [fourthTertile; hitPros(fourthIdx,:)];
 end
+%% Graph 1
+clf;
+quartileGraphs = tiledlayout(4,1,"TileSpacing","tight","Padding","tight");
+title(quartileGraphs,'Quartile of Mean Hit Profiles')
+%1
+nexttile
+plot(mean(firstTertile/2 + 0.5,1),color='b')
+title('First Quartile')
+ylim([0.475 0.515])
+%2
+nexttile
+plot(mean(secondTertile/2 + 0.5,1),Color='r')
+title('Second Quartile')
+ylim([0.475 0.515])
+%3
+nexttile
+plot(mean(thirdTertile/2 + 0.5,1),Color='g')
+title('Third Quartile')
+ylim([0.475 0.515])
+%4
+nexttile
+plot(mean(fourthTertile/2 + 0.5,1),Color='m')
+title('Fourth Quartile')
+ylim([0.475 0.515])
+
+%Graph 
+figure;
+hold on
+plot(mean(firstTertile/2 + 0.5,1),color='b')
+plot(mean(secondTertile/2 + 0.5,1),Color='r')
+plot(mean(thirdTertile/2 + 0.5,1),Color='g')
+plot(mean(fourthTertile/2 + 0.5,1),Color='m')
+hold off
+title('Quartile of Mean Hit Profiles')
+
+%% Bootstrapping
+
+%First Tertile
+for reps = 1:1000
+    bootfirstTertile(reps,1:800) = mean(datasample(firstTertile,size(firstTertile,1),"Replace",true));
+end
+
+%2nd Tertile
+for reps = 1:1000
+    bootsecondTertile(reps,1:800) = mean(datasample(secondTertile,size(secondTertile,1),"Replace",true));
+end
+
+%3rd Tertile
+for reps = 1:1000
+    bootthirdTertile(reps,1:800) = mean(datasample(thirdTertile,size(thirdTertile,1),"Replace",true));
+end
+
+%3rd Tertile
+for reps = 1:1000
+    bootfourthTertile(reps,1:800) = mean(datasample(fourthTertile,size(fourthTertile,1),"Replace",true));
+end
+
+%plot
+quartileGraphs = tiledlayout(4,1,"TileSpacing","tight","Padding","tight");
+title(quartileGraphs,'Quartile of Mean Hit Profiles')
+%1
+nexttile
+plot(mean(bootfirstTertile),color='b')
+title('First Quartile')
+ylim([0.475 0.515])
+%2
+nexttile
+plot(mean(bootsecondTertile),Color='r')
+title('Second Quartile')
+ylim([0.475 0.515])
+%3
+nexttile
+plot(mean(bootthirdTertile),Color='g')
+title('Third Quartile')
+ylim([0.475 0.515])
+%4
+nexttile
+plot(mean(bootfourthTertile),Color='m')
+title('Fourth Quartile')
+ylim([0.475 0.515])
+
 end
 
